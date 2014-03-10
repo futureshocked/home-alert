dd16e2a79445bbb20429354b38bced2dfbe1e0c8
Peter Dalmaris <peter@futureshock.com.au>
2014-03-10 10:41:19 +0000

    Added production environment.

--- web.rb
+++ web.rb
@@ -1,24 +1,25 @@
 require 'sinatra'
 configure do
     require 'redis'    
-    # uri = URI.parse(ENV["REDISCLOUD_URL"])
-    # $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    configure(:production){  
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+     }
+    configure(:development){ redis = Redis.new } 
+    
     set :server, :puma
 end
 
 
 get '/' do
 	erb :index
-
-
-	# redis = Redis.new
 	# redis.set("mykey", "hello world2")
 	# redis.get("mykey")
   # "Hello, world\n" 
 end
 
 post '/post_message' do
-	redis = Redis.new
+	# redis = Redis.new
 	# redis.set("dmd_id", params[:element_1])
 	redis.set("message", params[:element_2])
 	# redis.set("buzzer", params[:buzzer])
@@ -27,7 +28,7 @@ post '/post_message' do
 end
 
 get '/get_message' do
-	redis = Redis.new
+	# redis = Redis.new
 	redis.get("message") + "\n"
 end
 
